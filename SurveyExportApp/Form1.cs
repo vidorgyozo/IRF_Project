b7182de127ec5d6dc78f90f0f7d4c691542c11d6
Győző Vidor <gyozovidor@gmail.com>
2021-01-03 16:54:17 +0100

    Loaded answer and image list.

--- SurveyExportApp/Form1.cs
+++ SurveyExportApp/Form1.cs
@@ -12,9 +12,21 @@ namespace SurveyExportApp
 {
     public partial class Form1 : Form
     {
+        IRF_PROJECT_DB_COPYEntities context = new IRF_PROJECT_DB_COPYEntities();
+        List<SurveyAnswer> Answers;
+        List<Image> Images;
+
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Answers = context.SurveyAnswers.ToList();
+            Images = context.Images.ToList();
         }
     }
 }
